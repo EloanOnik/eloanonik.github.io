474ad81acfc1fbe71852a7609ece8fabe72bd794
EloanOnik <161943768+EloanOnik@users.noreply.github.com>
2025-07-27 14:42:12 +0400

    Update qr.js

--- home/js/qr.js
+++ home/js/qr.js
@@ -60,9 +60,9 @@ var btn = document.getElementById("modalBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // Открываем модальное окно при клике на кнопку
-btn.onclick = function() {
+btn.addEventListener("click", function() {
     modal.style.display = "block";
-}
+});
 
 // Закрываем модальное окно при клике на крестик
 span.onclick = function() {
